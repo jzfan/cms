dcf8e8c0157cb79c195fa2563854e4e6e2f4d74a
jzfan <jzmcc@163.com>
2020-01-03 09:34:05 +0800

    delete some assets

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -6,11 +6,11 @@
         <div class="col-md-6 col-lg-4 g-flex-centered g-theme-bg-blue-dark-v1">
           <div class="text-center g-color-gray-light-v2 g-pa-30">
             <div class="text-uppercase u-heading-v2-4--bottom g-brd-primary g-mb-40">
-              <h4 class="h6 g-font-weight-800 g-font-size-12 g-letter-spacing-2 g-color-primary g-mb-20">关于我们</h4>
+              <h4 class="h6 g-font-weight-800 g-font-size-12 g-letter-spacing-2 g-color-primary g-mb-20">关 于 我 们</h4>
               <h2
                 class="h1 u-heading-v2__title g-line-height-1_3 g-font-weight-600 g-font-size-40 g-color-white g-mb-minus-10">
-                共同&nbsp;&nbsp;&nbsp;&nbsp;创造
-                <br> 价值</h2>
+                共 同&nbsp;&nbsp;&nbsp;&nbsp;创 造
+                <br> 价 值</h2>
             </div>
 
             <p class="g-color-gray-light-v2 mb-0">Sed feugiat porttitor nunc, non dignissim ipsum vestibulum in. Donec
@@ -27,7 +27,7 @@
               <img class="img-fluid w-100" src="img/right.jpg" alt="Image description">
 
               <div class="g-pa-30">
-                <h3 class="text-uppercase g-font-weight-700 g-font-size-20 g-color-white g-mb-10">创建于 2019</h3>
+                <h3 class="text-uppercase g-font-weight-700 g-font-size-20 g-color-white g-mb-10">过去的一年 2019</h3>
                 <p class="g-color-gray-light-v2 g-mb-10">Etiam consectetur placerat gravida. Pellentesque ultricies
                   mattis est, quis elementum neque pulvinar at.</p>
                 <p class="g-color-gray-light-v2 mb-0">Aenean odio ante, varius vel tempor sed Ut condimentum ex ac enim
@@ -39,7 +39,7 @@
               <img class="img-fluid w-100" src="assets/img-temp/450x300/img2.jpg" alt="Image description">
 
               <div class="g-pa-30">
-                <h3 class="text-uppercase g-font-weight-700 g-font-size-20 g-color-white g-mb-10">Past 2012</h3>
+                <h3 class="text-uppercase g-font-weight-700 g-font-size-20 g-color-white g-mb-10">过去的一年 2019</h3>
                 <p class="g-color-gray-light-v2 g-mb-10">Etiam consectetur placerat gravida. Pellentesque ultricies
                   mattis est, quis elementum neque pulvinar at.</p>
                 <p class="g-color-gray-light-v2 mb-0">Aenean odio ante, varius vel tempor sed Ut condimentum ex ac enim
